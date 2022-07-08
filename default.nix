274ade8861a2433c5487d4a29342cbd717ce64bd
JuanM04 <me@juanm04.com>
2022-07-07 22:23:01 -0300

    test overlay

--- default.nix
+++ default.nix
@@ -1,17 +1,9 @@
-{pkgs ? import <nixpkgs> { inherit system; }, system ? builtins.currentSystem}:
+self: super:
 
-let
-    allPkgs = pkgs // railwayPkgs;
-
-    callPackage = path: overrides:
-        let f = import path;
-        in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
-
-    railwayPkgs = with pkgs; {
-      "npm-6.x" = callPackage ./pkgs/npm-6.x { };
-      "npm-8.x" = callPackage ./pkgs/npm-8.x { };
-      "pnpm-6.x" = callPackage ./pkgs/pnpm-6.x { };
-      "pnpm-7.x" = callPackage ./pkgs/pnpm-7.x { };
-      "yarn-1.x" = callPackage ./pkgs/yarn-1.x { };
-    };
-in railwayPkgs
+{
+  "npm-6.x" = super.callPackage ./pkgs/npm-6.x { };
+  "npm-8.x" = super.callPackage ./pkgs/npm-8.x { };
+  "pnpm-6.x" = super.callPackage ./pkgs/pnpm-6.x { };
+  "pnpm-7.x" = super.callPackage ./pkgs/pnpm-7.x { };
+  "yarn-1.x" = super.callPackage ./pkgs/yarn-1.x { };
+}
