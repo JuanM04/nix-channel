e43fd303a1b93231193779faaee2aa56a53a6aad
JuanM04 <me@juanm04.com>
2022-07-07 22:27:06 -0300

    Renamed packages

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 self: super:
 
 {
-  "npm-6.x" = super.callPackage ./pkgs/npm-6.x { };
-  "npm-8.x" = super.callPackage ./pkgs/npm-8.x { };
-  "pnpm-6.x" = super.callPackage ./pkgs/pnpm-6.x { };
-  "pnpm-7.x" = super.callPackage ./pkgs/pnpm-7.x { };
-  "yarn-1.x" = super.callPackage ./pkgs/yarn-1.x { };
+  npm-6_x = super.callPackage ./pkgs/npm-6_x { };
+  npm-8_x = super.callPackage ./pkgs/npm-8_x { };
+  pnpm-6_x = super.callPackage ./pkgs/pnpm-6_x { };
+  pnpm-7_x = super.callPackage ./pkgs/pnpm-7_x { };
+  yarn-1_x = super.callPackage ./pkgs/yarn-1_x { };
 }
